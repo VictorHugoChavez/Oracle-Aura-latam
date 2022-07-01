e888c55d93e06cb22cf3758c0f4b5502205cbcf3
VictorHugoChavez <96146923+VictorHugoChavez@users.noreply.github.com>
2022-06-30 20:23:38 -0500

    repaso JS

---
+++ JAVASCRIPTPRIMEROSPASOS/1er-modulo/primerprograma.js
@@ -0,0 +1,26 @@
+console.log("Hola Mundo");
+
+console.log(2+2);
+const nombre="Leornardo"
+
+const edad=24;
+console.log(edad);
+
+const nombrePadre="Antonio"
+const nombreMadre="Lucero"
+const nombre="Victor"
+
+console.log("Mi nombre es +" + nombre + " mi madre es " + nombreMadre)
+
+const nombrePadrex="Antonios"
+const nombreMadrex="Luceros"
+const nombrex="Victors"
+
+
+const edadMadre=55;
+const pi=3.1416;
+const esSoltero=true;
+let isBoleans=false;
+console.log(!isBoleans);
+
+
