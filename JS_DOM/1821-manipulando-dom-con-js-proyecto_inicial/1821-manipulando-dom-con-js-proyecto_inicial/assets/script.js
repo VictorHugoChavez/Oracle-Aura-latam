39d4ec7ad0b75d07cefc1bc39bd8ea786a15c62f
VictorHugoChavez <96146923+VictorHugoChavez@users.noreply.github.com>
2022-07-07 15:28:08 -0500

    Se suben cambios del archivo script

--- JS_DOM/1821-manipulando-dom-con-js-proyecto_inicial/1821-manipulando-dom-con-js-proyecto_inicial/assets/script.js
+++ JS_DOM/1821-manipulando-dom-con-js-proyecto_inicial/1821-manipulando-dom-con-js-proyecto_inicial/assets/script.js
@@ -1,33 +1,56 @@
-const btn = document.querySelector("[data-form-btn]")
-console.log(btn);
-
-const accion = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement('li');
-    task.classList.add("card");
-    const taskContet = document.createElement('div');
-    taskContet.appendChild(chechkComplete());
-    const tittleTask = document.createElement("span");
-    tittleTask.classList.add("task");
-    tittleTask.innerText = value;
-    taskContet.appendChild(tittleTask);
-    const info = ` 
+(() => {
+    const btn = document.querySelector("[data-form-btn]")
+    console.log(btn);
+    ///////////////////////////////////////////////////////////////////
+    const accion = (evento) => {
+        evento.preventDefault();
+        const input = document.querySelector("[data-form-input]");
+        const value = input.value;
+        const list = document.querySelector("[data-list]");
+        const task = document.createElement('li');
+        task.classList.add("card");
+        const taskContet = document.createElement('div');
+        taskContet.appendChild(chechkComplete());
+        const tittleTask = document.createElement("span");
+        tittleTask.classList.add("task");
+        tittleTask.innerText = value;
+        taskContet.appendChild(tittleTask);
+        taskContet.appendChild(deleteBtn());
+        const info = ` 
                 <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    console.log(info)
-    input.value = "";
-    task.appendChild(taskContet)
-    list.appendChild(task)
-}
-btn.addEventListener("click", accion);
+        console.log(info)
+        input.value = "";
+        task.appendChild(taskContet)
+        task.appendChild(deleteBtn());
+        list.appendChild(task)
+    }
+    btn.addEventListener("click", accion);
+    ///////////////////////////////////////////////////////////////
+    const chechkComplete = () => {
+            const i = document.createElement('i');
+            i.classList.add("far", "fa-check-square", "icon");
+            i.addEventListener("click", completeTask);
+            return i;
+        }
+        ///////////////////////////////////////////////////////////////
+    const completeTask = (event) => {
+        console.log(event.target)
+        const element = event.target;
+        element.classList.toggle("fas");
+        element.classList.toggle("completeIcon");
+        element.classList.toggle("far");
 
-const chechkComplete = () => {
-    const i = document.createElement('i');
-    i.classList.add("far");
-    i.classList.add("fa-check-square");
-    i.classList.add("icon");
+    };
 
-    return i;
-}
+    const deleteBtn = () => {
+        const i = document.createElement("i")
+        i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon")
+        i.addEventListener("click", deleteTask)
+        return i;
+    };
+
+    const deleteTask = (event) => {
+        const parent = event.target.parentElement;
+        parent.remove();
+    }
+})();
